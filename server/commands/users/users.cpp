d56740b2e76a156d8c4a9a8d62785816f91d3515
Yannislrg <yannis.lorang@epitech.eu>
2026-04-15 18:27:16 +0200

    fix(users): update unauthorized response message and improve user data formatting

--- server/commands/users/users.cpp
+++ server/commands/users/users.cpp
@@ -9,17 +9,19 @@
 #include "client.hpp"
 #include "database/Database.hpp"
 #include "server.hpp"
+#include "utils.hpp"
 
 void UsersCommand::execute(Client& client, Server& server) {
   if (client.getUserUuid().empty()) {
-    Server::sendToClient("530 Please login first\r\n", client);
+    Server::sendToClient("401 UNAUTHORIZED\r\n", client);
     return;
   }
   const auto& users = server.getDb().getUsers();
   Server::sendToClient("210-BEGIN USERS\r\n", client);
   for (const auto& user : users) {
-    Server::sendToClient("210 " + user.getUuid() + " " + user.getName() + " " +
-                             std::to_string(user.isConnected() ? 1 : 0) +
+    Server::sendToClient("210 " + Utils::quoteProtocolField(user.getUuid()) +
+                             " " + Utils::quoteProtocolField(user.getName()) +
+                             " " + std::to_string(user.isConnected() ? 1 : 0) +
                              "\r\n",
                          client);
   }
